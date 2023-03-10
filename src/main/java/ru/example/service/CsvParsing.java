d5b92edd606f8f6cbf8a2b571379b91f07169fec
DESKTOP-FIBDM79\danis <danisza4@gmail.com>
2023-03-11 02:16:35 +0300

    update

--- src/main/java/ru/example/service/CsvParsing.java
+++ src/main/java/ru/example/service/CsvParsing.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface CsvParsing {
-    String fileName = "C:\\Users\\danis\\Desktop\\airports-search-\\src\\main\\resources\\airports.csv";
+    String fileName = "src\\main\\resources\\airports.csv";
     Map<String, List<Long>> readAllFile(Integer columNumber);
 
     String[] readLine(Long n);
